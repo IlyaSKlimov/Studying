475631e082382040f407786aaa369294464d0d6a
IlyaSKlimov <il.klimov1999@gmail.com>
2020-07-10 16:33:44 +0300

    My first commit

---
+++ src/Introduction.java
@@ -0,0 +1,14 @@
+public class Introduction {
+    public static void main(String[] args){
+        System.out.print("Hello! my name is Ilya. ");
+        System.out.print("Hello! my name is Ilya.");
+        System.out.print("Hello! my name is Ilya. ");
+        System.out.print("Hello! my name is Ilya. ");
+        System.out.println("Hello! my name is Ilya. ");
+        System.out.println("I want to be java developer!");
+        System.out.println("I want to be java developer!");
+        System.out.println("I want to be java developer!");
+        System.out.println("I want to be java developer!");
+        System.out.println("I want to be java developer!");
+    }
+}
